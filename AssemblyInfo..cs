773c3eac5149b027ceaa9743bb95d43cbb2fc02d
SuleGeredelioglu <sulegeredelioglu@gmail.com>
2025-08-11 09:01:40 +0300

    Remove previous version

--- AssemblyInfo..cs
+++
@@ -1,5 +0,0 @@
-//Temel amaç: Derleme hakkýnda bilgi vermek, derlemeye özel ayarlarý yapmak.
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Tests")] // (test eriþimi için)
